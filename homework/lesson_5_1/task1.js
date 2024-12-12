5879fc08727c0265442a39a68cd6e5116193a5bc
vadym-usk <vadym.usk@gmail.com>
2024-12-12 22:45:15 +0200

    homework done

---
+++ homework/lesson_5_1/task1.js
@@ -0,0 +1,13 @@
+const averageGrade = 100;
+
+if (averageGrade < 60) {
+    console.log('Unsatisfactory');
+} else if (averageGrade <= 70) {
+    console.log('Satisfactory');
+} else if (averageGrade <= 80) {
+    console.log('Good');
+} else if (averageGrade <= 90) {
+    console.log('Very Good');
+} else if (averageGrade <= 100) {
+    console.log('Excellent');
+}
